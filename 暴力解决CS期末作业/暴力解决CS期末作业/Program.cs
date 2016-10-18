92b403a02fced7936ff0a3d5b47fdc6c48679048
Tianwu <2652017679@qq.com>
2016-10-18 19:58:50 +0800

    V2.0
    1.优化了复制，交叉，和变异运算，最后变异运算发现还是原来写的好
    2.运用BYTE替代INT
    3.貌似效率并未提高很多

---
+++ 暴力解决CS期末作业/暴力解决CS期末作业/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 暴力解决CS期末作业
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double x1 = 0;
+            //double x2 = 0;
+            //double max = 0;
+            //double precision = 0.001;
+
+            //for(double i=-3.0 ; i<12.1 ; i+= precision)
+            //    for (double j = 4.1; j < 5.8; j += precision)
+            //    {
+            //        double temp = 21.5 + i * Math.Sin(4 * Math.PI * i) + j * Math.Sin(20 * Math.PI * j);
+            //        if(temp > max)
+            //        {
+            //            max = temp;
+            //            x1 = i;
+            //            x2 = j;
+            //        }
+            //    }
+            //Console.WriteLine(x1+" "+x2+" "+max);
+            //Console.ReadKey();
+
+            //double i = 11.6259999999988; double j = 5.72500000000054;
+            double i = 2.55224317890496; double j = 4.5476306790425;
+            double temp = 21.5 + i * Math.Sin(4 * Math.PI * i) + j * Math.Sin(20 * Math.PI * j);
+            Console.WriteLine(temp);
+            Console.ReadKey();
+        }
+    }
+}
