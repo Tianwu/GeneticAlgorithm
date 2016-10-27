544fa6f30a4cde0407c7e3b29a8be518cdf70b4d
Tianwu <2652017679@qq.com>
2016-10-27 16:06:43 +0800

    V3.0
    1.对V2.0作1000次循环，故程序会运行很久，也不是很久，最终的结果也会更接近正确答案，个人计算了一下，提高了0.953%%
    
    2.其他的没有做很大改进，之前想用精度更高的DECIMAL，然而发现不好，因为SIN等函数都是DOUBLE类型的，这样强制类型转换，估计更浪费时间
    3.没有在算法上浪费时间了，只是在程序上优化。PS:老师改题目了，所以这道题相当于练手了，哈哈。

---
+++ CS期末作业-Finall/CS期末作业/FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CS期末作业
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void butnOK_Click(object sender, EventArgs e)
+        {
+            int M = Convert.ToInt32(txtBoxM.Text);
+            int T = Convert.ToInt32(txtBoxT.Text);
+            double Pc = Convert.ToDouble(txtBoxPc.Text);
+            double Pm = Convert.ToDouble(txtBoxPm.Text);
+            double[] output = null;
+            double temp = 0;
+            GeneticAlgorithm test = new GeneticAlgorithm(M, T, Pc, Pm);
+            for (int i = 0; i < 1000; ++i)
+            {
+                if (test.Solveproblem()[2] > temp)
+                    output = test.Solveproblem();
+            }
+            txtBoxX1.Text = Convert.ToString(output[0]);
+            txtBoxX2.Text = Convert.ToString(output[1]);
+            txtBoxAns.Text = Convert.ToString(output[2]);
+        }
+
+        private void butnCancle_Click(object sender, EventArgs e)
+        {
+           txtBoxM.Text = "";
+           txtBoxT.Text = "";
+           txtBoxPc.Text = "";
+           txtBoxPm.Text = "";
+        }
+
+    }
+}
